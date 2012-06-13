fbbfd49081f9ea645b36d1f0adc977dbf9e0c473
Radim Daniel Pánek <rdpanek@gmail.com>
2012-06-13 14:59:45 +0200

    pre-commit: changed support php pre-commit on windows

---
+++ framework/Hooks/pre-commit.php
@@ -0,0 +1,15 @@
+<?php
+
+include("./libs/PHPTestCase/framework/Hooks/HookTest.php");
+
+/**
+ * Pred metodou run, muzete nastavit dalsi parametry pro PHPUnit
+ * <code>
+ * $hk->setOptions("--group unit"); // pro spusteni testu v dane skupine
+ * $hk->setPath("php"); // cestu k php
+ * </code>
+ */
+$hk = new PHPTestCase\Framework\HookTest;
+$hk->setOptions("--group unit");
+$hk->setPath("php38");
+$hk->run();
